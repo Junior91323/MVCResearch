c542dcb1b4a53bdb2b4a176ecfb34c2639cf389a
Junior91323 <eugeneshabel@gmail.com>
2016-11-29 17:52:18 +0300

    Init Project

---
+++ MVCResearch/MVCResearch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCResearch.Startup))]
+namespace MVCResearch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
